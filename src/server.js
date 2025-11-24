bbeccb89e7b53a565650c6b9349af909661b2920
11StarLord <diogowalter114@gmail.com>
2025-11-24 22:42:16 +0100

    Adding the server and home routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const	express = require('express');
 const	app = express();
 
-app.get('/', (req, res) => {
-	res.send('Meu primeiro servidor!');
-});
+const homeRoute = require('./routes/home');
+
+app.use('/', homeRoute);
 
 app.listen(3000, () => {
 	console.log('Servidor rodando em http://localhost:3000');
