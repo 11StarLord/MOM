bf75ded1c8753ea4044b6b1a15a7961a2d145405
11StarLord <diogowalter114@gmail.com>
2025-12-09 20:08:08 +0100

    Correcting notesController path

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,6 @@
 const	express = require('express');
 const	router = express.Router();
-const	notesController = require('src/controllers/noteController');
+const	notesController = require('../controllers/notesController');
 
 router.get('/', notesController.getAllNotes);
 
